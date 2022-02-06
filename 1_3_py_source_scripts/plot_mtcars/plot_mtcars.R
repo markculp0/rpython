b801a1aea227d71011bd4ffc3913715ca72f892c
Mark Culp <markculp0@gmail.com>
2022-02-06 10:33:30 -0600

    plot mtcars

--- 1_3_py_source_scripts/plot_mtcars/plot_mtcars.R
+++ 1_3_py_source_scripts/plot_mtcars/plot_mtcars.R
@@ -1,7 +1,21 @@
 # Source a Python script that returns a Python DataFrame()
 
+library(tidyverse)
 library(reticulate)
 
 source_python("plot_mtcars.py")
+
+df <- mtplot()
+
+ggplot(data = df, mapping = aes(x=cyl, y=mpg, group=cyl)) +
+  geom_boxplot() + 
+  ggtitle("Car milage as a function of cylinders") +
+  xlab("number of cylinders")
+
+ggplot(data = df, mapping = aes(x=wt, y=mpg)) +
+  geom_point() +
+  geom_smooth() + 
+  ggtitle("Car milage as a function of weight") +
+  xlab("weight")
 
 
